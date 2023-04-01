80a315f7a671e0b0cfe0058f8b699de8cd1b5ecf
amansarafvt <amansaraf28@gmail.com>
2023-03-31 21:40:13 -0400

    Sign In

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+// import { useEffect, React } from "react"; //Redirect
+// import { getRedirectResult } from "firebase/auth"; //Redirect
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+  //   auth, //Redirect
+  //   signInWithGoogleRedirect, //Redirect
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  //Login with Google PopUp
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect Password!!");
+          break;
+        case "auth/user-not-found":
+          alert("Account Doesn't Exist!!");
+          break;
+        default:
+          console.log("User Error", error);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an Account?</h2>
+      <span>Sign in with email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+          {/*<Button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+  </Button>*/}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
+
+//Login With Google Redirect
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await getRedirectResult(auth);
+//       if (response) {
+//         const userDocRef = await createUserDocumentFromAuth(response.user);
+//       }
+//     }
+//     fetchData();
+//   }, []);
