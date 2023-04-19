82999a1419e55d1fbfb0b8130ca4d80f3a8baf05
Aman Saraf <amansaraf28@gmail.com>
2023-04-19 16:38:29 -0400

    category Page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,27 +1,3 @@
-// import { useContext } from "react";
-// import { CategoriesContext } from "../../context/categories.context";
-
-// import CategoryPreview from "../../components/category-preview/category-preview.component";
-
-// import "./shop.styles.scss";
-
-// const Shop = () => {
-//   const { categoriesMap } = useContext(CategoriesContext);
-
-//   return (
-//     <div className="shop-container">
-//       {Object.keys(categoriesMap).map((title) => {
-//         const products = categoriesMap[title];
-//         return (
-//           <CategoryPreview key={title} title={title} products={products} />
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default Shop;
-
 import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
